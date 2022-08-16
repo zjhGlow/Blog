99faef6397963045a3e8859d7b2210b41b43205a
Glow_In_The_Dark <94846923+zjhGlow@users.noreply.github.com>
2022-08-16 18:45:24 +0800

    Add files via upload

---
+++ src/resources/textures.js
@@ -0,0 +1,66 @@
+let bolgUrl = '../Blog/src/jsm'
+// let bolgUrl = '../src/jsm'
+
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = bolgUrl+'/terpSolutions.png';
+billboardTextures.bagHolderBetsTexture =
+  bolgUrl+'/Bagholdersbetsbillboard.png';
+billboardTextures.homeSweetHomeTexture =
+  bolgUrl+'/home-sweet-home-portrait.png';
+
+//box textures
+
+let boxTexture = {};
+boxTexture.Github = bolgUrl+'/githubLogo.png';
+boxTexture.twitter = bolgUrl+'/twitter.png';
+boxTexture.LinkedIn = bolgUrl+'/linkedInLogo.png';
+boxTexture.mail = bolgUrl+'/envelope.png';
+boxTexture.globe = bolgUrl+'/thunder.png';
+boxTexture.reactIcon = bolgUrl+'/react.png';
+boxTexture.allSkills = bolgUrl+'/allSkills.png';
+boxTexture.lensFlareMain = bolgUrl+'/lensflare0.png';
+boxTexture.skrillex = bolgUrl+'/skrillex.png';
+boxTexture.edmText = bolgUrl+'/EDM.png';
+boxTexture.writing = bolgUrl+'/writing.png';
+
+//material textures
+let stoneTexture = bolgUrl+'/stone.png';
+let woodTexture = bolgUrl+'/woodTexture.jpg';
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = bolgUrl+'/terp-solutions-text.png';
+inputText.activities = bolgUrl+'/activities_text.png';
+inputText.bagholderBetsText = bolgUrl+'/bagholderbets-text.png';
+inputText.homeSweetHomeText = bolgUrl+'/home-sweet-home-text.png';
+inputText.staticPortfolio = bolgUrl+'/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = bolgUrl+'/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions =
+  'https://web.archive.org/web/20200302001846/https://terpsolutions.com/';
+URL.ryanfloyd = 'https://ryanfloyd.io';
+URL.bagholderBets = 'https://bagholder-bets.herokuapp.com';
+URL.homeSweetHomeURL = 'https://github.com/MrRyanFloyd/home-sweet-127.0.0.1';
+URL.gitHub = 'https://github.com/zjhGlow';
+URL.twitter = '';
+URL.LinkedIn = '';
+URL.email = 'zjh_glow@163.com';
+URL.githubBagholder = 'https://github.com/MrRyanFloyd/bagholder-bets';
+URL.githubHomeSweetHome = 'https://github.com/MrRyanFloyd/home-sweet-127.0.0.1';
+URL.devTo =
+  'https://zjhglow.github.io/GLOW/';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
